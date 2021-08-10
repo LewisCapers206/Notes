7d343525f16154928422ba94f2baf7fdf4ab7e08
Lewis Capers <lewiscapersjr206@gmail.com>
2021-08-09 20:37:24 -0700

    final

--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,25 @@
-const { CONNREFUSED } = require('dns');
 const express = require('express');
 const path = require('path');
-const routes = require('./routes/routes');
-const PORT = process.env.PORT || 3001;
+const routes = require('../routes/routes');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api/notes', routes);
 
-app.get('/notes', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get('/notes', function(req,res) {
+  res.sendFile(path.join(__dirname,'/public/notes.html'));
 });
 
 
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
+// Starts the server to begin listening
 app.listen(PORT, () =>
-    console.log('App listening at https://localhost:${port}')
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
